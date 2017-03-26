f5e32acd5564900a6f00f8f3da87c4cb682e90bf
Celsuss <jenslorden@gmail.com>
2017-03-26 19:21:26 +0200

    Added ue4 project

---
+++ Source/NomadsEnd/NomadsEndGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "NomadsEndGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOMADSEND_API ANomadsEndGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

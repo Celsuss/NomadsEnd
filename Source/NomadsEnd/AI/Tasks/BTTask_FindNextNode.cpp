7ef64b2083f993d8f596ad8dc38063316dc1ebdf
Celsuss <jenslorden@gmail.com>
2017-04-23 19:31:24 +0200

    Folder changes

--- Source/NomadsEnd/AI/Tasks/BTTask_FindNextNode.cpp
+++ Source/NomadsEnd/AI/Tasks/BTTask_FindNextNode.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "NomadsEnd.h"
-#include "PathNode.h"
-#include "PathFollowerAIController.h"
+#include "AI/PathNode.h"
+#include "AI/PathFollowerAIController.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"

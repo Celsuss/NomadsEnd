0d5a760a442c11558cc19c14e401083b9438e059
Lihte <ful_gnagare@hotmail.com>
2017-05-13 21:29:07 +0200

    Added removed AI stuff

---
+++ Source/NomadsEnd/Private/AI/ConstructingInterface.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NomadsEnd.h"
+#include "ConstructingInterface.h"
+
+
+// This function does not need to be modified.
+UConstructingInterface::UConstructingInterface(const class FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+// Add default functionality here for any IConstructingInterface functions that are not pure virtual.
